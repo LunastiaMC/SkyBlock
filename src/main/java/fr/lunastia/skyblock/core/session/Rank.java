6ee90784df157e7704b46a2bba0cd058a88a4db6
Gaëtan H <steellgold0@gmail.com>
2022-08-01 01:09:24 +0200

    Application des permissions au joueur

--- src/main/java/fr/lunastia/skyblock/core/session/Rank.java
+++ src/main/java/fr/lunastia/skyblock/core/session/Rank.java
@@ -1,4 +1,18 @@
 package fr.lunastia.skyblock.core.session;
 
-public record Rank(int id, String name, String coloredName, String arrow) {
+import fr.lunastia.skyblock.core.Core;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachment;
+
+import java.util.ArrayList;
+
+public record Rank(int id, String name, String coloredName, String arrow, ArrayList<String> permissions) {
+
+    public void applyPermissions(Player player) {
+        PermissionAttachment attachment = player.addAttachment(Core.getInstance());
+        for (String permission : permissions) {
+            System.out.println("[Skyblock] " + player.getName() + " has permission " + permission);
+            attachment.setPermission(permission, true);
+        }
+    }
 }
