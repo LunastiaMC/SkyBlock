29d47851bd0228a0f0dccc9c5051aa7a7abb802a
Gaëtan H <steellgold0@gmail.com>
2022-07-31 19:17:50 +0200

    Ajout du préfix principal

--- src/main/java/fr/lunastia/skyblock/core/utils/ColorUtil.java
+++ src/main/java/fr/lunastia/skyblock/core/utils/ColorUtil.java
@@ -11,6 +11,8 @@ import java.util.regex.Pattern;
 public class ColorUtil {
 
     public static final String HELP = "&#d0d0d0&l[&#d9d9d9&lA&#e2e2e2&li&#ececec&ld&#f5f5f5&le&#fefefe&l]§r§7";
+    public static final String PREFIX = "&#229dc3&l[&#2796c6&lS&#2b8ec9&lk&#3087cc&ly&#357fcf&lB&#3978d1&ll&#3e70d4&lo&#4369d7&lc&#4761da&lk&#4c5add&l]§r§7";
+
     public static String colorize(String message) {
         message = matchHexaReplace("&#", message);
         message = matchHexaReplace("#", message);
