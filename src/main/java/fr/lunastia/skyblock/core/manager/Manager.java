36cb5dbd6017c5e58dc13f03da7f27ac78f9b7b6
Gaëtan H <steellgold0@gmail.com>
2022-07-31 19:18:33 +0200

    Ajout des différents gestionnaires

--- src/main/java/fr/lunastia/skyblock/core/manager/Manager.java
+++ src/main/java/fr/lunastia/skyblock/core/manager/Manager.java
@@ -1,10 +1,26 @@
 package fr.lunastia.skyblock.core.manager;
+
 import fr.lunastia.skyblock.core.database.DatabaseManager;
+
 public class Manager {
+    private static SessionManager sessionManager;
+    private static RankManager rankManager;
     private static DatabaseManager databaseManager;
+
     public static void init() {
+        sessionManager = new SessionManager();
         databaseManager = new DatabaseManager();
+        rankManager = new RankManager();
     }
+
+    public static SessionManager getSessionManager() {
+        return sessionManager;
+    }
+
+    public static RankManager getRankManager() {
+        return rankManager;
+    }
+
     public static DatabaseManager getDatabaseManager() {
         return databaseManager;
     }
