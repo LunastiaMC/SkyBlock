06cb1ae7a6dde8a57595bcaf244ede324c0d174a
Gaëtan H <steellgold0@gmail.com>
2022-07-31 19:58:29 +0200

    Classe -> Record

--- src/main/java/fr/lunastia/skyblock/core/session/Rank.java
+++ src/main/java/fr/lunastia/skyblock/core/session/Rank.java
@@ -1,31 +1,4 @@
 package fr.lunastia.skyblock.core.session;
 
-public class Rank {
-    private final int id;
-    private final String name;
-    private final String coloredName;
-    private final String arrow;
-
-    public Rank(int id, String name, String coloredName, String arrow) {
-        this.id = id;
-        this.name = name;
-        this.coloredName = coloredName;
-        this.arrow = arrow;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getColoredName() {
-        return coloredName;
-    }
-
-    public String getArrow() {
-        return arrow;
-    }
+public record Rank(int id, String name, String coloredName, String arrow) {
 }
