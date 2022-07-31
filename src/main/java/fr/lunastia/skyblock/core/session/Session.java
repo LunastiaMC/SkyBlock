5af22a4932295813211998b5fd8c2e9af026b17d
Gaëtan H <steellgold0@gmail.com>
2022-08-01 01:09:35 +0200

    Classe de la session

--- src/main/java/fr/lunastia/skyblock/core/session/Session.java
+++ src/main/java/fr/lunastia/skyblock/core/session/Session.java
@@ -1,5 +1,28 @@
 package fr.lunastia.skyblock.core.session;
 
+import fr.lunastia.skyblock.core.manager.Manager;
+import fr.lunastia.skyblock.core.manager.RankManager;
+import org.bukkit.entity.Player;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Session {
-    // TODO
+
+    private final Player player;
+    private final Rank rank;
+
+    public Session(Player player, ResultSet rs) throws SQLException {
+        this.player = player;
+        this.rank = Manager.getRankManager().getRank(rs.getInt("rank"));
+        this.rank.applyPermissions(player);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
 }
