2746b4b37b0ad7f8de770c2494980ea6fe4ef2ee
Gaëtan H <steellgold0@gmail.com>
2022-07-31 15:56:58 +0200

    Ajout d'une fonction pour récupérer l'instance du plugin

--- src/main/java/fr/lunastia/skyblock/core/Core.java
+++ src/main/java/fr/lunastia/skyblock/core/Core.java
@@ -1,16 +1,25 @@
 package fr.lunastia.skyblock.core;
 
+import fr.lunastia.skyblock.core.listeners.PlayerListeners;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
+    public static Core instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         getLogger().info("Skyblock is now enabled !");
 
         this.getServer().getPluginManager().registerEvents(new PlayerListeners(), this);
     }
+
     @Override
     public void onDisable() {
         getLogger().info("Skyblock is now disabled !");
+    }
+
+    public static Core getInstance() {
+        return instance;
     }
 }
