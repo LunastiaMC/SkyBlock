fed4dfb80a903edce5e36bf67f6a1aede280a896
K_iMi <51194216+LeKiMi@users.noreply.github.com>
2022-08-01 18:10:36 +0200

    Ajouts  couleurs announce
    Ajouts des couleurs Skyblock, Rouge, Bleu, Verte, Orange

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/AnnounceCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/AnnounceCommand.java
@@ -13,9 +13,14 @@ import org.bukkit.entity.Player;
 @Permission("skyblock.announce.command")
 public class AnnounceCommand {
     @Default
-    public static void announce(Player player, @AMultiLiteralArgument({"good", "bad", "help"}) String type, @AGreedyStringArgument String message) {
+    public static void announce(Player player, @AMultiLiteralArgument({"green", "red", "blue", "skyblock", "orange", "help"}) String type, @AGreedyStringArgument String message) {
         String prefix = ColorUtil.colorize(switch (type) {
-            case "green" -> "&#2db725&l[&#30b62c&lA&#32b633&lN&#35b53a&lN&#37b441&lO&#3ab348&lN&#3cb34f&lC&#3fb256&lE&#41b15d&l]";
+            case "green" -> "&#079809&l[ &#079009&lS&#068808&lk&#068008&ly&#067807&lb&#057107&ll&#056906&lo&#056106&lc&#045905&lk &#045105&l]";
+            case "red" -> "&#8e0000&l[ &#8e0000&lS&#8e0000&lk&#8e0000&ly&#8e0000&lb&#8e0000&ll&#8e0000&lo&#8e0000&lc&#8e0000&lk &#8e0000&l]";
+            case "skyblock" -> "&#04267f&l[ &#0d2f7f&lS&#16387e&lk&#1f417e&ly&#284a7e&lb&#31537d&ll&#3a5c7d&lo&#43657d&lc&#4c6e7c&lk &#55777c&l]";
+            case "blue" -> "&#000098&l[ &#070f99&lS&#0f1d99&lk&#162c9a&ly&#1e3b9a&lb&#25499b&ll&#2d589b&lo&#34679c&lc&#3c759c&lk &#43849d&l]";
+            case "orange" -> "&#985b33&l[ &#915731&lS&#8a532e&lk&#834e2c&ly&#7c4a2a&lb&#744627&ll&#6d4225&lo&#663d23&lc&#5f3920&lk &#58351e&l]";
+            
             default -> ColorUtil.PREFIX;
         });
 
@@ -26,3 +31,9 @@ public class AnnounceCommand {
         });
     }
 }
+
+// Couleur Skyblock : &#04267f&l[ &#0d2f7f&lS&#16387e&lk&#1f417e&ly&#284a7e&lb&#31537d&ll&#3a5c7d&lo&#43657d&lc&#4c6e7c&lk &#55777c&l]
+// Couleur Rouge : &#8e0000&l[ &#8e0000&lS&#8e0000&lk&#8e0000&ly&#8e0000&lb&#8e0000&ll&#8e0000&lo&#8e0000&lc&#8e0000&lk &#8e0000&l]
+// Couleur Bleu : &#000098&l[ &#070f99&lS&#0f1d99&lk&#162c9a&ly&#1e3b9a&lb&#25499b&ll&#2d589b&lo&#34679c&lc&#3c759c&lk &#43849d&l]
+// Couleur Verte : &#079809&l[ &#079009&lS&#068808&lk&#068008&ly&#067807&lb&#057107&ll&#056906&lo&#056106&lc&#045905&lk &#045105&l]
+// Couleur Orange : &#985b33&l[ &#915731&lS&#8a532e&lk&#834e2c&ly&#7c4a2a&lb&#744627&ll&#6d4225&lo&#663d23&lc&#5f3920&lk &#58351e&l]
