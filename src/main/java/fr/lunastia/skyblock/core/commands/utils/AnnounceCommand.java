54658c8844ca1c59a9333833b22dfb7f7d5eb2b4
Gaëtan H <steellgold0@gmail.com>
2022-08-01 18:42:36 +0200

    Modifications des couleurs

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/AnnounceCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/AnnounceCommand.java
@@ -13,26 +13,31 @@ import org.bukkit.entity.Player;
 @Permission("skyblock.announce.command")
 public class AnnounceCommand {
     @Default
-    public static void announce(Player player, @AMultiLiteralArgument({"green", "red", "blue", "skyblock", "orange", "help"}) String type, @AGreedyStringArgument String message) {
+    public static void announce(Player player, @AMultiLiteralArgument({"green", "red", "blue", "aqua", "orange", "yellow", "white"}) String type, @AGreedyStringArgument String message) {
         String prefix = ColorUtil.colorize(switch (type) {
-            case "green" -> "&#079809&l[ &#079009&lS&#068808&lk&#068008&ly&#067807&lb&#057107&ll&#056906&lo&#056106&lc&#045905&lk &#045105&l]";
-            case "red" -> "&#8e0000&l[ &#8e0000&lS&#8e0000&lk&#8e0000&ly&#8e0000&lb&#8e0000&ll&#8e0000&lo&#8e0000&lc&#8e0000&lk &#8e0000&l]";
-            case "blue" -> "&#000098&l[ &#070f99&lS&#0f1d99&lk&#162c9a&ly&#1e3b9a&lb&#25499b&ll&#2d589b&lo&#34679c&lc&#3c759c&lk &#43849d&l]";
-            case "orange" -> "&#985b33&l[ &#915731&lS&#8a532e&lk&#834e2c&ly&#7c4a2a&lb&#744627&ll&#6d4225&lo&#663d23&lc&#5f3920&lk &#58351e&l]";
-            
+            case "green" ->
+                    "&#18ff7b&l[&#1dfc7b&lA&#22f87c&lN&#27f57c&lN&#2df27c&lO&#32ee7c&lN&#37eb7d&lC&#3ce77d&lE&#41e47d&l] §r§7";
+            case "red" ->
+                    "&#ff3c3c&l[&#fc3d3d&lA&#f83d3d&lN&#f53e3e&lN&#f23f3f&lO&#ee3f3f&lN&#eb4040&lC&#e74040&lE&#e44141&l] §r§7";
+            case "blue" ->
+                    "&#314cff&l[&#334ffc&lA&#3551f8&lN&#3754f5&lN&#3956f2&lO&#3b59ee&lN&#3d5beb&lC&#3f5ee7&lE&#4160e4&l] §r§7";
+            case "aqua" ->
+                    "&#31d4ff&l[&#33ccfc&lA&#35c3f8&lN&#37bbf5&lN&#39b3f2&lO&#3baaee&lN&#3da2eb&lC&#3f99e7&lE&#4191e4&l] §r§7";
+            case "orange" ->
+                    "&#ff5739&l[&#fc5839&lA&#f85939&lN&#f55a39&lN&#f25b3a&lO&#ee5b3a&lN&#eb5c3a&lC&#e75d3a&lE&#e45e3a&l] §r§7";
+            case "yellow" ->
+                    "&#e6ff1b&l[&#e4fc20&lA&#e1f825&lN&#dff52a&lN&#ddf22f&lO&#daee34&lN&#d8eb39&lC&#d5e73e&lE&#d3e443&l] §r§7";
+            case "white" ->
+                    "&#ffecec&l[&#fce9e9&lA&#f8e6e6&lN&#f5e3e3&lN&#f2e1e1&lO&#eedede&lN&#ebdbdb&lC&#e7d8d8&lE&#e4d5d5&l] §r§7";
             default -> ColorUtil.PREFIX;
         });
 
         String coloredText = ColorUtil.colorize(message);
 
         Bukkit.getOnlinePlayers().forEach(p -> {
+            p.sendMessage(" ");
             p.sendMessage(prefix + coloredText);
+            p.sendMessage(" ");
         });
     }
 }
-
-// Couleur Skyblock : &#04267f&l[ &#0d2f7f&lS&#16387e&lk&#1f417e&ly&#284a7e&lb&#31537d&ll&#3a5c7d&lo&#43657d&lc&#4c6e7c&lk &#55777c&l]
-// Couleur Rouge : &#8e0000&l[ &#8e0000&lS&#8e0000&lk&#8e0000&ly&#8e0000&lb&#8e0000&ll&#8e0000&lo&#8e0000&lc&#8e0000&lk &#8e0000&l]
-// Couleur Bleu : &#000098&l[ &#070f99&lS&#0f1d99&lk&#162c9a&ly&#1e3b9a&lb&#25499b&ll&#2d589b&lo&#34679c&lc&#3c759c&lk &#43849d&l]
-// Couleur Verte : &#079809&l[ &#079009&lS&#068808&lk&#068008&ly&#067807&lb&#057107&ll&#056906&lo&#056106&lc&#045905&lk &#045105&l]
-// Couleur Orange : &#985b33&l[ &#915731&lS&#8a532e&lk&#834e2c&ly&#7c4a2a&lb&#744627&ll&#6d4225&lo&#663d23&lc&#5f3920&lk &#58351e&l]
