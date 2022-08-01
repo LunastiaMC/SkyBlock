18ed8cac1f3164761854c06c1743c4d0553db203
Gaëtan H <steellgold0@gmail.com>
2022-08-02 01:29:27 +0200

    Ajout de la base du système d'économie

--- src/main/java/fr/lunastia/skyblock/core/session/Session.java
+++ src/main/java/fr/lunastia/skyblock/core/session/Session.java
@@ -13,19 +13,22 @@ public class Session {
 
     private final Player player;
     private final Rank rank;
+    private final Long money;
 
     public Session(Player player, ResultSet rs) throws SQLException {
         this.player = player;
         this.rank = Manager.getRankManager().getRank(rs.getInt("rank"));
+        this.money = rs.getLong("money");
         this.rank.applyPermissions(player);
 
         NametagEdit.getApi().setPrefix(player, ColorUtil.colorize(this.getRank().nametagName()) + "§7");
         player.setPlayerListName(ColorUtil.colorize(this.getRank().nametagName()) + player.getName());
     }
 
-    public Session(Player player, Integer rank) {
+    public Session(Player player, Integer rank, Long money) {
         this.player = player;
         this.rank = Manager.getRankManager().getRank(rank);
+        this.money = money;
         this.rank.applyPermissions(player);
     }
 
@@ -35,5 +38,9 @@ public class Session {
 
     public Rank getRank() {
         return rank;
+    }
+
+    public Long getMoney() {
+        return money;
     }
 }
