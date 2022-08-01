f72542976aa516b32fc73564640564fa046095ef
Gaëtan H <steellgold0@gmail.com>
2022-08-01 21:53:43 +0200

    Temporairement

--- src/main/java/fr/lunastia/skyblock/core/listeners/PlayerListeners.java
+++ src/main/java/fr/lunastia/skyblock/core/listeners/PlayerListeners.java
@@ -1,9 +1,11 @@
 package fr.lunastia.skyblock.core.listeners;
 
 import fr.lunastia.skyblock.core.manager.Manager;
+import fr.lunastia.skyblock.core.session.Session;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -21,6 +23,13 @@ public class PlayerListeners implements Listener {
     public void onPlayerQuit(PlayerQuitEvent event) throws SQLException {
         Player player = event.getPlayer();
         Manager.getSessionManager().saveSession(player);
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        Session session = Manager.getSessionManager().getSession(player);
+        event.setFormat("§7[§6" + session.getRank().nametagName() + "§7] §f" + player.getName() + "§7: " + event.getMessage());
     }
 
     @EventHandler
