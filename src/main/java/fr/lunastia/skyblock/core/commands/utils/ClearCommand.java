7b23a12680c758db45468ff9a892116e1b53e9f7
Gaëtan H <steellgold0@gmail.com>
2022-08-02 18:37:23 +0200

    Modifications mineurs (français + indentation)

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/ClearCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/ClearCommand.java
@@ -3,28 +3,24 @@ package fr.lunastia.skyblock.core.commands.utils;
 import dev.jorel.commandapi.annotations.Command;
 import dev.jorel.commandapi.annotations.Default;
 import dev.jorel.commandapi.annotations.Permission;
-import dev.jorel.commandapi.annotations.arguments.AGreedyStringArgument;
 import dev.jorel.commandapi.annotations.arguments.APlayerArgument;
 import fr.lunastia.skyblock.core.utils.ColorUtil;
-import org.bukkit.Bukkit;
-import org.bukkit.Server;
 import org.bukkit.entity.Player;
 
 @Command("clear")
 @Permission("skyblock.clear.command")
 public class ClearCommand {
     @Default
-    public static void clear(Player player , @APlayerArgument Player playerargs)
-    {
-        final int inventairesize = playerargs.getInventory().getContents().length;
-        playerargs.getInventory().clear();
-        ColorUtil.sendMessage(player, "Vous venez de vider l'inventaire de " + playerargs.getName() + " qui contenait un total de " + inventairesize + " d'items", ColorUtil.PREFIX);
+    public static void clear(Player player, @APlayerArgument Player target) {
+        final int inventorySize = target.getInventory().getContents().length;
+        target.getInventory().clear();
+        ColorUtil.sendMessage(player, "Vous venez de vider l'inventaire de §f" + target.getName() + " §7qui contenait un total de §f" + inventorySize + " items", ColorUtil.PREFIX);
     }
+
     @Default
-    public static void clear(Player player)
-    {
-        final int inventairesize = player.getInventory().getContents().length;
+    public static void clear(Player player) {
+        final int inventorySize = player.getInventory().getContents().length;
         player.getInventory().clear();
-        ColorUtil.sendMessage(player, "Vous venez de vider votre l'inventaire qui contenait un total de " + inventairesize + " d'items", ColorUtil.PREFIX);
+        ColorUtil.sendMessage(player, "Vous venez de vider votre inventaire qui contenait un total de §f" + inventorySize + " §7items", ColorUtil.PREFIX);
     }
 }
