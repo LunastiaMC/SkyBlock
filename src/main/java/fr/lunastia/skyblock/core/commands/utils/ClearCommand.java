3cbd5b6589e8a7e6e5b319c414c61e73fe860f04
Gaëtan H <steellgold0@gmail.com>
2022-08-02 18:48:42 +0200

    Correctif: Affichage du nombre d'item

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/ClearCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/ClearCommand.java
@@ -7,20 +7,30 @@ import dev.jorel.commandapi.annotations.arguments.APlayerArgument;
 import fr.lunastia.skyblock.core.utils.ColorUtil;
 import org.bukkit.entity.Player;
 
+import java.util.Objects;
+
 @Command("clear")
 @Permission("skyblock.clear.command")
 public class ClearCommand {
     @Default
     public static void clear(Player player, @APlayerArgument Player target) {
-        final int inventorySize = target.getInventory().getContents().length;
+        ColorUtil.sendMessage(player, "Vous venez de vider l'inventaire de §f" + target.getName() + " §7qui contenait un total de §f" + getCount(target) + " items", ColorUtil.PREFIX);
         target.getInventory().clear();
-        ColorUtil.sendMessage(player, "Vous venez de vider l'inventaire de §f" + target.getName() + " §7qui contenait un total de §f" + inventorySize + " items", ColorUtil.PREFIX);
     }
 
     @Default
     public static void clear(Player player) {
-        final int inventorySize = player.getInventory().getContents().length;
+        ColorUtil.sendMessage(player, "Vous venez de vider votre inventaire qui contenait un total de §f" + getCount(player) + " §7items", ColorUtil.PREFIX);
         player.getInventory().clear();
-        ColorUtil.sendMessage(player, "Vous venez de vider votre inventaire qui contenait un total de §f" + inventorySize + " §7items", ColorUtil.PREFIX);
+    }
+
+    private static int getCount(Player player) {
+        int count = 0;
+        for (int i = 0; i < player.getInventory().getSize(); i++) {
+            if (player.getInventory().getItem(i) != null) {
+                count += Objects.requireNonNull(player.getInventory().getItem(i)).getAmount();
+            }
+        }
+        return count;
     }
 }
