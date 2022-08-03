e51cb1a9a906f560d8f18ac6037fe86749ba7f9c
Gaëtan H <steellgold0@gmail.com>
2022-08-03 15:11:37 +0200

    Ajout du hat (Item dans la main)

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/HatCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/HatCommand.java
@@ -8,6 +8,7 @@ import dev.jorel.commandapi.annotations.Subcommand;
 import fr.lunastia.skyblock.core.gui.HatListGUI;
 import fr.lunastia.skyblock.core.manager.Manager;
 import fr.lunastia.skyblock.core.utils.ColorUtils;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 @Command("hat")
@@ -17,27 +18,22 @@ public class HatCommand {
     public static void hat(Player player) {
         Manager.getGUIManager().open(player, HatListGUI.class);
 
-        // TODO: Faire une commande avec autre permission pour utiliser un bloc
-        //if (player.getItemInHand().getType() != Material.AIR)
-        //{
-        //if (player.getInventory().getHelmet() != null)
-        //{
-        //ItemStack item = player.getInventory().getHelmet().clone();
-        //player.getInventory().addItem(item);
-        //player.getInventory().removeItem(player.getInventory().getHelmet());
-        //player.getInventory().setHelmet(player.getInventory().getItemInMainHand());
-        //player.getInventory().removeItem(player.getInventory().getItemInMainHand());
-        //ColorUtil.sendMessage(player, "Vous venez de mettre l'item " + player.getInventory().getHelmet().getItemMeta().getDisplayName() + " en tant que chapeau !", ColorUtil.HAT);
-        //} else
-        //{
-        //player.getInventory().setHelmet(player.getInventory().getItemInMainHand());
-        //player.getInventory().removeItem(player.getInventory().getItemInMainHand());
-        //ColorUtil.sendMessage(player, "Vous venez de mettre l'item " + player.getInventory().getHelmet().getItemMeta().getDisplayName() + " en tant que chapeau !", ColorUtil.HAT);
-        //}
-        //} else
-        //{
-        //ColorUtil.sendMessage(player, "Vous ne pouvez pas effectuer cette commande en ayant la main vide !", ColorUtil.HAT);
-        //}
+    }
+
+    @Subcommand("hand")
+    @Permission("skyblock.hat.hand.command")
+    public static void hand(Player player) {
+        if (player.getItemInHand().getType() != Material.AIR) {
+            if (player.getInventory().getHelmet() == null) {
+                player.getInventory().setHelmet(player.getInventory().getItemInMainHand());
+                player.getInventory().removeItem(player.getInventory().getItemInMainHand());
+                ColorUtils.sendMessage(player, "Vous venez de mettre l'item §d" + player.getInventory().getHelmet().getItemMeta().getDisplayName() + " §7en tant que chapeau !", ColorUtils.HAT);
+            } else {
+                ColorUtils.sendMessage(player, "Vous avez déjà un chapeau !", ColorUtils.HAT);
+            }
+        } else {
+            ColorUtils.sendMessage(player, "Vous ne pouvez pas effectuer cette commande en ayant la main vide !", ColorUtils.HAT);
+        }
     }
 
     @Subcommand("remove")
@@ -49,6 +45,8 @@ public class HatCommand {
             } else {
                 ColorUtils.sendMessage(player, "Vous devez avoir un chapeau pour executer cette commande !", ColorUtils.HAT);
             }
+        } else {
+            ColorUtils.sendMessage(player, "Vous n'avez pas d'item dans votre case \"§dCasque§7\" !", ColorUtils.HAT);
         }
     }
 }
