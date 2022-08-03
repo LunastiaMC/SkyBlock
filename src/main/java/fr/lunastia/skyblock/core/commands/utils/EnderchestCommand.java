f8b76dde0d1cae2bb97edb7e5923b8a3c26137c5
Gaëtan H <steellgold0@gmail.com>
2022-08-03 23:30:19 +0200

    Test

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/EnderchestCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/EnderchestCommand.java
@@ -1,6 +1,9 @@
 package fr.lunastia.skyblock.core.commands.utils;
 
-import dev.jorel.commandapi.annotations.*;
+import dev.jorel.commandapi.annotations.Alias;
+import dev.jorel.commandapi.annotations.Command;
+import dev.jorel.commandapi.annotations.Default;
+import dev.jorel.commandapi.annotations.Permission;
 import dev.jorel.commandapi.annotations.arguments.APlayerArgument;
 import fr.lunastia.skyblock.core.utils.ColorUtils;
 import org.bukkit.entity.Player;
@@ -8,7 +11,7 @@ import org.bukkit.entity.Player;
 @Command("enderchest")
 @Alias("ec")
 @Permission("skyblock.ec.command")
-public class EnderChestCommand {
+public class EnderchestCommand {
     @Default
     public static void enderchest(Player player) {
         player.openInventory(player.getEnderChest());
@@ -18,7 +21,7 @@ public class EnderChestCommand {
     @Permission("skyblock.ec.other.command")
     public static void see(Player player, @APlayerArgument Player target) {
         if (target.getName().equals(player.getName())) {
-            ColorUtils.sendMessage(player, "Vous ne pouvez ouvrir votre propre inventaire !", ColorUtils.PREFIX,true);
+            ColorUtils.sendMessage(player, "Vous ne pouvez ouvrir votre propre inventaire !", ColorUtils.PREFIX, true);
             return;
         }
 
