fdc2caba74fe951e166469598cd6b315f640b5ee
Gaëtan H <steellgold0@gmail.com>
2022-08-03 14:19:58 +0200

    Correctif du /fly

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/FlyCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/FlyCommand.java
@@ -7,6 +7,8 @@ import fr.lunastia.skyblock.core.utils.ColorUtils;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+
 @Command("fly")
 public class FlyCommand {
     @Default
@@ -16,14 +18,16 @@ public class FlyCommand {
             ColorUtils.sendMessage(player, "Vous ne pouvez pas effectuer cette commande en créatif !", ColorUtils.PREFIX);
             return;
         }
+
         if (player.getWorld().getName().equals(player.getUniqueId() + "") || player.isOp()) {
-            if (player.isFlying()) {
-                player.setFlying(false);
-                ColorUtils.sendMessage(player, "Vous venez de désactiver le fly !", ColorUtils.PREFIX);
-            } else {
-                player.setFlying(true);
-                ColorUtils.sendMessage(player, "Vous devez d'activer le fly !", ColorUtils.PREFIX);
-            }
+            HashMap<Boolean, String> map = new HashMap<>();
+            map.put(false, "d'§factiver§7");
+            map.put(true, "de §fdésactiver§7");
+
+            boolean fly = player.getAllowFlight();
+            player.setAllowFlight(!fly);
+            if (!fly) player.setFlying(false);
+            ColorUtils.sendMessage(player, "Vous venez " + map.get(fly) + " le §fvol §7!", ColorUtils.PREFIX);
         } else {
             ColorUtils.sendMessage(player, "Vous devez effectuer cette commande dans votre île !", ColorUtils.PREFIX);
         }
