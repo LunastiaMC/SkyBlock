14ac14766fcfaa9b16ab4747835034dc0fe4d28d
Gaëtan H <steellgold0@gmail.com>
2022-08-03 18:12:08 +0200

    Ajout du /enderchest <player>

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/EnderchestCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/EnderchestCommand.java
@@ -1,17 +1,27 @@
 package fr.lunastia.skyblock.core.commands.utils;
 
-import dev.jorel.commandapi.annotations.Alias;
-import dev.jorel.commandapi.annotations.Command;
-import dev.jorel.commandapi.annotations.Default;
-import dev.jorel.commandapi.annotations.Permission;
+import dev.jorel.commandapi.annotations.*;
+import dev.jorel.commandapi.annotations.arguments.APlayerArgument;
+import fr.lunastia.skyblock.core.utils.ColorUtils;
 import org.bukkit.entity.Player;
 
 @Command("enderchest")
 @Alias("ec")
 @Permission("skyblock.ec.command")
-public class EnderchestCommand {
+public class EnderChestCommand {
     @Default
     public static void enderchest(Player player) {
         player.openInventory(player.getEnderChest());
+    }
+
+    @Default
+    @Permission("skyblock.ec.see.command")
+    public static void see(Player player, @APlayerArgument Player target) {
+        if (target.getName().equals(player.getName())) {
+            ColorUtils.sendMessage(player, "Vous ne pouvez ouvrir votre propre inventaire !", ColorUtils.PREFIX,true);
+            return;
+        }
+
+        player.openInventory(target.getEnderChest());
     }
 }
