fb374188abd7ab140e960a549e07dbabe4965f72
Gaëtan H <steellgold0@gmail.com>
2022-08-04 13:43:50 +0200

    Ajout de nouveaux évènements au GUI's

--- src/main/java/fr/lunastia/skyblock/core/manager/GUIManager.java
+++ src/main/java/fr/lunastia/skyblock/core/manager/GUIManager.java
@@ -8,6 +8,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -47,6 +49,30 @@ public class GUIManager implements Listener {
                         throw new RuntimeException(e);
                     }
                     event.setCancelled(true);
+                });
+    }
+
+    @EventHandler
+    public void onOpen(InventoryOpenEvent event) {
+        Player player = (Player) event.getPlayer();
+        Inventory inventory = event.getInventory();
+
+        getRegisteredGUIs().values().stream()
+                .filter(menu -> event.getView().getTitle().equals(menu.getName()))
+                .forEach(gui -> {
+                    gui.onOpen(player, inventory);
+                });
+    }
+
+    @EventHandler
+    public void onClose(InventoryCloseEvent event) {
+        Player player = (Player) event.getPlayer();
+        Inventory inventory = event.getInventory();
+
+        getRegisteredGUIs().values().stream()
+                .filter(menu -> event.getView().getTitle().equals(menu.getName()))
+                .forEach(gui -> {
+                    gui.onClose(player, inventory);
                 });
     }
 
