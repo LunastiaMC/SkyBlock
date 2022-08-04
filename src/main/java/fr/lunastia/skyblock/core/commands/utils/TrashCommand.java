e073303e83e4f14c0ab3185e1d139e77c22bb70a
Gaëtan H <steellgold0@gmail.com>
2022-08-04 13:46:11 +0200

    Ajout d'un /trash

---
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/TrashCommand.java
@@ -0,0 +1,18 @@
+package fr.lunastia.skyblock.core.commands.utils;
+
+import dev.jorel.commandapi.annotations.Command;
+import dev.jorel.commandapi.annotations.Default;
+import dev.jorel.commandapi.annotations.Permission;
+import fr.lunastia.skyblock.core.gui.TrashGUI;
+import fr.lunastia.skyblock.core.manager.Manager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+@Command("trash")
+@Permission("skyblock.trash.command")
+public class TrashCommand {
+    @Default
+    public static void trash(Player player) {
+        Manager.getGUIManager().open(player, TrashGUI.class);
+    }
+}
