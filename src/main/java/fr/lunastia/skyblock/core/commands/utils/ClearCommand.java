20c026f8ba8fad901f4eb8ecc9b61f661dc56818
Gaëtan H <steellgold0@gmail.com>
2022-08-04 13:45:06 +0200

    Modification du /clear

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/ClearCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/ClearCommand.java
@@ -5,7 +5,9 @@ import dev.jorel.commandapi.annotations.Default;
 import dev.jorel.commandapi.annotations.Permission;
 import dev.jorel.commandapi.annotations.arguments.APlayerArgument;
 import fr.lunastia.skyblock.core.utils.ColorUtils;
+import fr.lunastia.skyblock.core.utils.ItemUtils;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 import java.util.Objects;
 
@@ -14,23 +16,13 @@ import java.util.Objects;
 public class ClearCommand {
     @Default
     public static void clear(Player player, @APlayerArgument Player target) {
-        ColorUtils.sendMessage(player, "Vous venez de vider l'inventaire de §f" + target.getName() + " §7qui contenait un total de §f" + getCount(target) + " items", ColorUtils.PREFIX);
+        ColorUtils.sendMessage(player, "Vous venez de vider l'inventaire de §f" + target.getName() + " §7qui contenait un total de §f" + ItemUtils.countInventory(target.getInventory()) + " items", ColorUtils.PREFIX);
         target.getInventory().clear();
     }
 
     @Default
     public static void clear(Player player) {
-        ColorUtils.sendMessage(player, "Vous venez de vider votre inventaire qui contenait un total de §f" + getCount(player) + " §7items", ColorUtils.PREFIX);
+        ColorUtils.sendMessage(player, "Vous venez de vider votre inventaire qui contenait un total de §f" + ItemUtils.countInventory(player.getInventory()) + " §7items", ColorUtils.PREFIX);
         player.getInventory().clear();
-    }
-
-    private static int getCount(Player player) {
-        int count = 0;
-        for (int i = 0; i < player.getInventory().getSize(); i++) {
-            if (player.getInventory().getItem(i) != null) {
-                count += Objects.requireNonNull(player.getInventory().getItem(i)).getAmount();
-            }
-        }
-        return count;
     }
 }
