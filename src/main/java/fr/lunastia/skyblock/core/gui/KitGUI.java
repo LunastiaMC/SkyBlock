64663dd339d3eac09ec19691521e47956a6306eb
Pralexkid <alexkid407@gmail.com>
2022-08-07 19:26:58 +0200

    Ajout de la commande /kit add (non fonctionnelle)

---
+++ src/main/java/fr/lunastia/skyblock/core/gui/KitGUI.java
@@ -0,0 +1,45 @@
+package fr.lunastia.skyblock.core.gui;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.sql.SQLException;
+
+public class KitGUI implements GUIBuilder{
+    @Override
+    public String getName() {
+        return "Kit List";
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void getContents(Player player, Inventory inventory) {
+
+    }
+
+    @Override
+    public void onClick(Player player, Inventory inventory, ItemStack itemStack, int slot, ClickType clickType) throws SQLException {
+
+    }
+
+    @Override
+    public void onClose(Player player, Inventory inventory) {
+
+    }
+
+    @Override
+    public void onOpen(Player player, Inventory inventory) {
+
+    }
+
+    @Override
+    public boolean clickCancelled() {
+        return false;
+    }
+}
