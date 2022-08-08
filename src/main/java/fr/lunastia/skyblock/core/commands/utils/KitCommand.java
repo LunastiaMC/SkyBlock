07d3642dd13238dd202982493caa5b23b0f7fc81
Gaëtan H <steellgold0@gmail.com>
2022-08-08 13:43:51 +0200

    Modification de la commande

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/KitCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/KitCommand.java
@@ -3,8 +3,11 @@ package fr.lunastia.skyblock.core.commands.utils;
 
 import dev.jorel.commandapi.annotations.Command;
 import dev.jorel.commandapi.annotations.Default;
+import dev.jorel.commandapi.annotations.Permission;
 import dev.jorel.commandapi.annotations.Subcommand;
 import dev.jorel.commandapi.annotations.arguments.AGreedyStringArgument;
+import dev.jorel.commandapi.annotations.arguments.AIntegerArgument;
+import dev.jorel.commandapi.annotations.arguments.AStringArgument;
 import fr.lunastia.skyblock.core.gui.KitGUI;
 import fr.lunastia.skyblock.core.manager.Manager;
 import org.bukkit.entity.Player;
@@ -13,14 +16,13 @@ import java.sql.SQLException;
 
 @Command("kit")
 public class KitCommand {
-    public static String List;
-
     @Default
-    public static void kitList(Player player) {
+    public static void kit(Player player) {
         Manager.getGUIManager().open(player, KitGUI.class);
     }
 
     @Subcommand("add")
-    public static void addKIt(Player player, @AGreedyStringArgument String nom, @AGreedyStringArgument String permission) throws SQLException {
+    @Permission("skyblock.kit.add")
+    public static void add(Player player, @AStringArgument String identifier, @AIntegerArgument Integer headId, @AGreedyStringArgument String displayName) {
     }
 }
