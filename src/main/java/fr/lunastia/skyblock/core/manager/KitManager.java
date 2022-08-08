8bc4244a7857cda2a62bbb5124299e8a33d8dc39
Gaëtan H <steellgold0@gmail.com>
2022-08-08 13:29:24 +0200

    Modification de la requête SQL

--- src/main/java/fr/lunastia/skyblock/core/manager/KitManager.java
+++ src/main/java/fr/lunastia/skyblock/core/manager/KitManager.java
@@ -3,6 +3,7 @@ package fr.lunastia.skyblock.core.manager;
 
 import fr.lunastia.skyblock.core.session.Session;
 import fr.lunastia.skyblock.core.session.server.Kit;
+import fr.lunastia.skyblock.core.utils.BukkitSerialization;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -19,16 +20,12 @@ public class KitManager {
     public void addKit(String identifier, Kit kit) throws SQLException {
         Connection connection = Manager.getDatabaseManager().getDatabase().getConnection();
 
-        final PreparedStatement statementCreation = connection.prepareStatement("INSERT INTO sessions (uuid, rank, money, permissions, freezed, vanished) VALUES (?,?,?,?,?,?)");
-        statementCreation.setString(1, player.getUniqueId().toString());
-        statementCreation.setInt(2, Manager.getRankManager().getDefaultRank().id());
-        statementCreation.setLong(3, 0);
-        statementCreation.setString(4, "");
-        statementCreation.setBoolean(5, false);
-        statementCreation.setBoolean(6, false);
+        final PreparedStatement statementCreation = connection.prepareStatement("INSERT INTO kits (identifier, displayName, inventory64) VALUES (?,?,?)");
+        statementCreation.setString(1, kit.identifier());
+        statementCreation.setString(2, kit.displayName());
+        statementCreation.setString(3, BukkitSerialization.toBase64(kit.inventory()));
         statementCreation.execute();
 
-        final Session session = new Session(player, Manager.getRankManager().getDefaultRank().id(), 0L, new String[]{}, false, false);
-        this.sessions.put(player.getUniqueId().toString(), session);
+        this.kits.put(kit.identifier(), kit);
     }
 }
