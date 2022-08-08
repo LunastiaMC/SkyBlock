c29b8cd73c02d7c9eaa516d60a404c34be095ed6
Gaëtan H <steellgold0@gmail.com>
2022-08-08 13:39:18 +0200

    Ajout de la tête du kit

--- src/main/java/fr/lunastia/skyblock/core/session/server/Kit.java
+++ src/main/java/fr/lunastia/skyblock/core/session/server/Kit.java
@@ -1,12 +1,18 @@
 package fr.lunastia.skyblock.core.session.server;
 
+import fr.lunastia.skyblock.core.manager.Manager;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
-public record Kit(String identifier, String displayName, Inventory inventory) {
+public record Kit(String identifier, String displayName, int headId, Inventory inventory) {
     // TODO: Faire une fonction pour distribuer les objets de inventory
     //       Si il a la main de gauche vide par exemple alors sa lui applique l'objet de deuxième main de inventory
     //       Sinon, sa lui met dans son inventaire
     //       Et ainsi de suite pareil pour l'armure
     //       Avant de donner le kit vérifier si il a de la place, si il en à pas on drop a terre
     //       En lui notifiant qu'on a jeter a terre ces objets
+
+    public ItemStack getHead() {
+        return Manager.getHeadDatabaseAPI().getItemHead(String.valueOf(headId));
+    }
 }
