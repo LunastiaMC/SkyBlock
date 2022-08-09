f658aeeffdc49ebede5258b45e7068e89189bbd7
Gaëtan H <steellgold0@gmail.com>
2022-08-09 14:25:18 +0200

    Ajout d'un UUID dans la classe d'île

--- src/main/java/fr/lunastia/skyblock/core/session/Island.java
+++ src/main/java/fr/lunastia/skyblock/core/session/Island.java
@@ -1,4 +1,16 @@
 package fr.lunastia.skyblock.core.session;
 
+import java.util.UUID;
+
 public class Island {
+
+    private final UUID uuid;
+
+    public Island(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
 }
