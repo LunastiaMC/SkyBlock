039ea87ba6aeaa116efee54510a4b1e7ef7b1b3a
Gaëtan H <steellgold0@gmail.com>
2022-08-09 15:05:59 +0200

    Chargement, sauvegarde, et création de l'île (50%)

--- src/main/java/fr/lunastia/skyblock/core/manager/IslandManager.java
+++ src/main/java/fr/lunastia/skyblock/core/manager/IslandManager.java
@@ -1,5 +1,60 @@
 package fr.lunastia.skyblock.core.manager;
 
-public class IslandManager {
+import fr.lunastia.skyblock.core.session.Island;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class IslandManager {
+    private HashMap<String, Island> islands;
+
+    public IslandManager() {
+        this.islands = new HashMap<>();
+    }
+
+    public void loadIsland(String uuid) throws SQLException {
+        Connection connection = Manager.getDatabaseManager().getDatabase().getConnection();
+
+        final PreparedStatement statementFinder = connection.prepareStatement("SELECT * FROM sessions WHERE uuid = ?");
+        statementFinder.setString(1, uuid);
+        final ResultSet resultSet = statementFinder.executeQuery();
+
+        if (resultSet.next()) {
+            final Island island = new Island(uuid);
+            this.islands.put(island.getUuid(), new Island(uuid));
+        } else {
+            UUID newUUID = UUID.randomUUID();
+
+            final PreparedStatement statementCreation = connection.prepareStatement("INSERT INTO sessions (uuid, rank, money, permissions, island, freezed, vanished) VALUES (?,?,?,?,?,?,?)");
+            statementCreation.setString(1, newUUID.toString());
+            statementCreation.execute();
+
+            final Island island = new Island(newUUID.toString());
+            this.islands.put(newUUID.toString(), island);
+        }
+    }
+
+    public void saveIsland(Island island) throws SQLException {
+        Connection connection = Manager.getDatabaseManager().getDatabase().getConnection();
+
+        try {
+            final PreparedStatement statement = connection.prepareStatement("UPDATE island SET uuid = ? WHERE uuid = ?");
+            statement.setString(1, island.getUuid());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<String, Island> getIslands() {
+        return islands;
+    }
+
+    public Island getIsland(String uuid) {
+        return islands.get(uuid);
+    }
 }
