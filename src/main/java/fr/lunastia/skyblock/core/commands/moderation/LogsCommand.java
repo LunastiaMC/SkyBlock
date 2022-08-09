fae50ffedf7b3b52abd12c79d473e0f1bcfe3cec
Gaëtan H <steellgold0@gmail.com>
2022-08-09 19:14:03 +0200

    Ajout du /logs

---
+++ src/main/java/fr/lunastia/skyblock/core/commands/moderation/LogsCommand.java
@@ -0,0 +1,16 @@
+package fr.lunastia.skyblock.core.commands.moderation;
+
+import dev.jorel.commandapi.annotations.Command;
+import dev.jorel.commandapi.annotations.Default;
+import dev.jorel.commandapi.annotations.Permission;
+import dev.jorel.commandapi.annotations.arguments.AStringArgument;
+import org.bukkit.entity.Player;
+
+@Command("logs")
+@Permission("skyblock.logs.command")
+public class LogsCommand {
+    @Default
+    public void logs(Player player, @AStringArgument String target) {
+
+    }
+}
