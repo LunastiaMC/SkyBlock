aae8766f47eb3af698444ee40e2544f169e54fbb
Gaëtan H <steellgold0@gmail.com>
2022-08-10 15:22:08 +0200

    Ajout d'une classe Enum

---
+++ src/main/java/fr/lunastia/skyblock/core/utils/colors/Colors.java
@@ -0,0 +1,10 @@
+package fr.lunastia.skyblock.core.utils.colors;
+
+public enum Colors {
+    PREFIX("&#229dc3&l[&#2796c6&lS&#2b8ec9&lk&#3087cc&ly&#357fcf&lB&#3978d1&ll&#3e70d4&lo&#4369d7&lc&#4761da&lk&#4c5add&l]§r§7"),
+    HAT("&#fb5cf1&l[&#f058e6&lC&#e654da&lh&#db50cf&la&#d04cc3&lp&#db4dbf&le&#e74fbc&la&#f250b8&lu&#fd51b4&l]§r§7");
+
+    Colors(String s) {
+
+    }
+}
