ec11727325b4835e435b0738d049175a2b3cfd2b
Gaëtan H <steellgold0@gmail.com>
2022-08-10 16:04:58 +0200

    Ajout de la base du /ban

--- src/main/java/fr/lunastia/skyblock/core/commands/moderation/BanCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/moderation/BanCommand.java
@@ -1,9 +1,22 @@
 package fr.lunastia.skyblock.core.commands.moderation;
 
+import dev.jorel.commandapi.annotations.Command;
+import dev.jorel.commandapi.annotations.Default;
+import dev.jorel.commandapi.annotations.Permission;
+import dev.jorel.commandapi.annotations.arguments.AGreedyStringArgument;
+import dev.jorel.commandapi.annotations.arguments.AIntegerArgument;
+import dev.jorel.commandapi.annotations.arguments.AMultiLiteralArgument;
+import dev.jorel.commandapi.annotations.arguments.APlayerArgument;
+import fr.lunastia.skyblock.core.session.server.EnumLogs;
+import fr.lunastia.skyblock.core.session.server.logs.Log;
+import fr.lunastia.skyblock.core.session.server.logs.LogTypeModeration;
+import fr.lunastia.skyblock.core.utils.ColorUtils;
+import org.bukkit.entity.Player;
 
 @Command("ban")
 @Permission("skyblock.ban.command")
 public class BanCommand {
+    @Default
     public static void ban(Player player, @APlayerArgument Player target, @AMultiLiteralArgument({
             "hours",
             "days",
@@ -12,5 +25,17 @@ public class BanCommand {
             "years"
     }) String time, @AIntegerArgument(min = 1) int duration, @AGreedyStringArgument String reason
     ) {
+        ColorUtils.sendMessage(player, "Vous venez de bannir &#ff3c3c" + target.getName() + "§7 pour une durée de &#ff3c3c" + duration + " " + translateDuration(time) + "§7 pour la raison: &#ff3c3c" + reason,ColorUtils.MOD);
+    }
+
+    public static String translateDuration(String time) {
+        return switch (time) {
+            case "hours" -> "heures";
+            case "days" -> "jours";
+            case "weeks" -> "semaines";
+            case "months" -> "mois";
+            case "years" -> "années";
+            default -> "";
+        };
     }
 }
