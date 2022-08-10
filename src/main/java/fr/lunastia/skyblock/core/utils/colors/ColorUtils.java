dc488ed41e13064cdba12bb72e22cd9869b9b2c5
Gaëtan H <steellgold0@gmail.com>
2022-08-10 15:28:21 +0200

    Changement ColorUtils -> Colors

--- src/main/java/fr/lunastia/skyblock/core/utils/colors/ColorUtils.java
+++ src/main/java/fr/lunastia/skyblock/core/utils/colors/ColorUtils.java
@@ -11,7 +11,6 @@ import java.util.regex.Pattern;
 public class ColorUtils {
 
     public static final String HELP = "&#d0d0d0&l[&#d9d9d9&lA&#e2e2e2&li&#ececec&ld&#f5f5f5&le&#fefefe&l]§r§7";
-    public static final String PREFIX = "&#229dc3&l[&#2796c6&lS&#2b8ec9&lk&#3087cc&ly&#357fcf&lB&#3978d1&ll&#3e70d4&lo&#4369d7&lc&#4761da&lk&#4c5add&l]§r§7";
     public static final String DISCORD = "&#5865f2&l[&#586af2&lD&#5770f2&li&#5775f2&ls&#567af2&lc&#567ff2&lo&#5585f2&lr&#558af2&ld&#548ff2&l]§r§7";
     public static final String BANK = "&#fbf800&l[&#f6f004&lB&#f1e808&la&#ece00c&ln&#e7d70f&lq&#e2cf13&lu&#ddc717&le&#d8bf1b&l]§r§7";
     public static final String HAT = "&#fb5cf1&l[&#f058e6&lC&#e654da&lh&#db50cf&la&#d04cc3&lp&#db4dbf&le&#e74fbc&la&#f250b8&lu&#fd51b4&l]§r§7";
