6df9d650d7fa80e57c1052c92b9d00fb1a0adb30
Gaëtan H <steellgold0@gmail.com>
2022-08-10 13:29:52 +0200

    Ajout des logs en rapport avec la modération

--- src/main/java/fr/lunastia/skyblock/core/session/server/EnumLogs.java
+++ src/main/java/fr/lunastia/skyblock/core/session/server/EnumLogs.java
@@ -3,6 +3,14 @@ package fr.lunastia.skyblock.core.session.server;
 public enum EnumLogs {
     PLAYER_JOIN,
     PLAYER_QUIT,
+    PLAYER_KICKED,
+    PLAYER_BANNED,
+    PLAYER_IP_BANNED,
+    PLAYER_UNBANNED,
+    PLAYER_IP_UNBANNED,
+    PLAYER_MUTED,
+    PLAYER_UNMUTED,
+    PLAYER_MUTE_EXPIRED,
 
     EnumLogs(String action) {
     }
