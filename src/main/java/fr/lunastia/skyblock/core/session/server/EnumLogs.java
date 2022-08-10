432aa214088580b67203af344ea12422c2d0618b
Gaëtan H <steellgold0@gmail.com>
2022-08-10 13:29:22 +0200

    Modification

--- src/main/java/fr/lunastia/skyblock/core/session/server/EnumLogs.java
+++ src/main/java/fr/lunastia/skyblock/core/session/server/EnumLogs.java
@@ -1,11 +1,6 @@
 package fr.lunastia.skyblock.core.session.server;
 
 public enum EnumLogs {
-    KICK("kicked"),
-    BANNED("banned"),
-    PARDONNED("pardoned"),
-    IP_BANNED("banned_ip"),
-    IP_PARDONNED("pardoned_ip");
 
     EnumLogs(String action) {
     }
