222d9559458af78d787298029c588c5167b4b3f3
Gaëtan H <steellgold0@gmail.com>
2022-08-10 15:28:02 +0200

    Changement ColorUtils -> Colors

--- src/main/java/fr/lunastia/skyblock/core/commands/utils/FlyCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/utils/FlyCommand.java
@@ -4,6 +4,7 @@ import dev.jorel.commandapi.annotations.Command;
 import dev.jorel.commandapi.annotations.Default;
 import dev.jorel.commandapi.annotations.Permission;
 import fr.lunastia.skyblock.core.utils.colors.ColorUtils;
+import fr.lunastia.skyblock.core.utils.colors.Colors;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 
@@ -15,7 +16,7 @@ public class FlyCommand {
     @Permission("skyblock.fly.command")
     public static void fly(Player player) {
         if (player.getGameMode().equals(GameMode.CREATIVE)) {
-            ColorUtils.sendMessage(player, "Vous ne pouvez pas effectuer cette commande en créatif !", ColorUtils.PREFIX);
+            ColorUtils.sendMessage(player, "Vous ne pouvez pas effectuer cette commande en créatif !", Colors.PREFIX);
             return;
         }
 
@@ -27,9 +28,9 @@ public class FlyCommand {
             boolean fly = player.getAllowFlight();
             player.setAllowFlight(!fly);
             if (!fly) player.setFlying(false);
-            ColorUtils.sendMessage(player, "Vous venez " + map.get(fly) + " le §fvol §7!", ColorUtils.PREFIX);
+            ColorUtils.sendMessage(player, "Vous venez " + map.get(fly) + " le §fvol §7!", Colors.PREFIX);
         } else {
-            ColorUtils.sendMessage(player, "Vous devez effectuer cette commande dans votre île !", ColorUtils.PREFIX);
+            ColorUtils.sendMessage(player, "Vous devez effectuer cette commande dans votre île !", Colors.PREFIX);
         }
     }
 }
