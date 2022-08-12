08f8be98d6b020c7e612fa4b985baea6e8bce86e
Gaëtan H <steellgold0@gmail.com>
2022-08-12 11:28:18 +0200

    Ajout des messages de bannissement (a revoir)

--- src/main/java/fr/lunastia/skyblock/core/utils/colors/Colors.java
+++ src/main/java/fr/lunastia/skyblock/core/utils/colors/Colors.java
@@ -6,7 +6,10 @@ public enum Colors {
     BANK("&#fbf800&l[&#f6f004&lB&#f1e808&la&#ece00c&ln&#e7d70f&lq&#e2cf13&lu&#ddc717&le&#d8bf1b&l]§r§7"),
     TRASH("&#868484&l[&#888787&lP&#8b8a8a&lo&#8d8d8c&lu&#8f908f&lb&#929492&le&#949795&ll&#969a97&ll&#999d9a&le&#9ba09d&l]§r§7"),
     REPAIR("&#8f9873&l[&#919972&lE&#929a72&ln&#949b71&lc&#959c70&ll&#979d70&lu&#999e6f&lm&#9a9f6e&le &#9ca06e&la&#9da16d&lm&#9fa26d&lé&#a0a36c&ll&#a2a46b&li&#a4a56b&lo&#a5a66a&lr&#a7a769&lé&#a8a869&le&#aaa968&l]§r§7"),
-    MOD("&#ff3c3c&l[&#f43939&lS&#ea3737&la&#df3434&ln&#d53131&lc&#ca2f2f&lt&#c02c2c&li&#b52929&lo&#ab2727&ln&#a02424&l]");
+    MOD("&#ff3c3c&l[&#f43939&lS&#ea3737&la&#df3434&ln&#d53131&lc&#ca2f2f&lt&#c02c2c&li&#b52929&lo&#ab2727&ln&#a02424&l]"),
+
+    // Colors
+    MOD_RED("&#ff3c3c");
 
     private final String k;
 
