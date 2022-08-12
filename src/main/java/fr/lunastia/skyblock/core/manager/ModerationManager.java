0b916ea90ac3b01a0f15de9df31ca2ccd982b6ef
Gaëtan H <steellgold0@gmail.com>
2022-08-12 11:47:02 +0200

    Ajout de fonctions addBan, delBan et isBanned

--- src/main/java/fr/lunastia/skyblock/core/manager/ModerationManager.java
+++ src/main/java/fr/lunastia/skyblock/core/manager/ModerationManager.java
@@ -1,10 +1,28 @@
 package fr.lunastia.skyblock.core.manager;
 
+import org.bukkit.entity.Player;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ModerationManager {
+    public void addBan(Player player, String expire, String reason) {
 
+    }
 
+    public void delBan(String target) {
+        
+    }
 
+    public boolean isBanned(Player player) throws SQLException {
+        Connection connection = Manager.getDatabaseManager().getDatabase().getConnection();
 
+        final PreparedStatement statementFinder = connection.prepareStatement("SELECT * FROM banned WHERE uuid = ?");
+        statementFinder.setString(1, player.getUniqueId().toString());
+        final ResultSet resultSet = statementFinder.executeQuery();
+
+        return resultSet.next();
+    }
 }
