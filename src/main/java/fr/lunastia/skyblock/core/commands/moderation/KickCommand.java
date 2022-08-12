c565853ee93fdb0ff0e448685142c4c889f4b1e1
Gaëtan H <steellgold0@gmail.com>
2022-08-12 15:39:34 +0200

    Ajout du /kick

--- src/main/java/fr/lunastia/skyblock/core/commands/moderation/KickCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/moderation/KickCommand.java
@@ -5,8 +5,6 @@ import dev.jorel.commandapi.annotations.Default;
 import dev.jorel.commandapi.annotations.Permission;
 import dev.jorel.commandapi.annotations.arguments.AGreedyStringArgument;
 import dev.jorel.commandapi.annotations.arguments.APlayerArgument;
-import fr.lunastia.skyblock.core.session.server.EnumLogs;
-import fr.lunastia.skyblock.core.session.server.logs.LogTypeModeration;
 import fr.lunastia.skyblock.core.utils.colors.ColorUtils;
 import fr.lunastia.skyblock.core.utils.colors.Colors;
 import org.bukkit.entity.Player;
@@ -16,7 +14,14 @@ import org.bukkit.entity.Player;
 public class KickCommand {
     @Default
     public static void kick(Player player, @APlayerArgument Player target, @AGreedyStringArgument String reason) {
-        LogTypeModeration log = new LogTypeModeration(EnumLogs.PLAYER_KICKED, target, player, reason);
-        ColorUtils.sendMessage(player, log.getExpireAt().toString(), Colors.PREFIX);
+        ColorUtils.sendMessage(player, "Vous venez d'expulser le joueur " + Colors.MOD_RED.color() + target.getName() + " §7pour la raison: " + Colors.MOD_RED.color() + reason, Colors.PREFIX);
+        String[] message = new String[]{
+                ColorUtils.colorize(Colors.MOD_RED.color() + "§lVous êtes expulsé(e) du serveur\n\n"),
+                ColorUtils.colorize("§7§l➤§r§7 Pour la raison suivante: " + Colors.MOD_RED.color() + reason) + "\n",
+                "\n",
+                ColorUtils.colorize("§r§7Si vous pensez qu'il y a un abus de pouvoir") + "\n",
+                ColorUtils.colorize("§r§7vous pouvez ouvrir un ticket sur le discord") + "\n",
+                ColorUtils.colorize(Colors.DISCORD_COLOR.color() + "discord.gg/F9aQyQZxQr")
+        };
     }
 }
