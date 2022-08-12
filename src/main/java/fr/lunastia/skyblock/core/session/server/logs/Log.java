dc6b8ee613282e1d6d7527ad688fc6dff4d4c4de
Gaëtan H <steellgold0@gmail.com>
2022-08-12 22:45:24 +0200

    Ajout du "moderator_name"

--- src/main/java/fr/lunastia/skyblock/core/session/server/logs/Log.java
+++ src/main/java/fr/lunastia/skyblock/core/session/server/logs/Log.java
@@ -16,14 +16,15 @@ public interface Log {
         Core.getInstance().getServer().getScheduler().runTaskAsynchronously(Core.getInstance(), () -> {
             try {
                 Connection connection = Manager.getDatabaseManager().getDatabase().getConnection();
-                final PreparedStatement statement = connection.prepareStatement("INSERT INTO logs (type, target, target_name, moderator, reason, startAt, expireAt) VALUES (?,?,?,?,?,?,?)");
+                final PreparedStatement statement = connection.prepareStatement("INSERT INTO logs (type, target, target_name, moderator, moderator_name, reason, startAt, expireAt) VALUES (?,?,?,?,?,?,?,?)");
                 statement.setString(1, type.toString());
                 statement.setString(2, target.getUniqueId().toString());
                 statement.setString(3, target.getName());
                 statement.setString(4, moderator.getUniqueId().toString());
-                statement.setString(5, reason);
-                statement.setString(6, startAt);
-                statement.setString(7, expireAt);
+                statement.setString(5, moderator.getName());
+                statement.setString(6, reason);
+                statement.setString(7, startAt);
+                statement.setString(8, expireAt);
                 statement.execute();
             } catch (SQLException e) {
                 e.printStackTrace();
