c8a509ea3f07130beb0baafd111cd57bb8c9dc3a
Gaëtan H <steellgold0@gmail.com>
2022-08-12 19:12:48 +0200

    Ajout d'une tête ainsi qu'un type de danger

--- src/main/java/fr/lunastia/skyblock/core/session/server/EnumLogs.java
+++ src/main/java/fr/lunastia/skyblock/core/session/server/EnumLogs.java
@@ -5,9 +5,27 @@ import fr.lunastia.skyblock.core.utils.colors.Colors;
 import java.util.ArrayList;
 
 public enum EnumLogs {
+    PLAYER_KIT_USE("", "Utilisation d'un équipement", Colors.COMMON,51459),
 
     PLAYER_OPEN_INVENTORY("", "Ouverture d'un inventaire", Colors.COMMON,227),
 
+    KIT_ADDED("", "Ajout d'un équipement", Colors.WARNING,51459),
+    KIT_REMOVED("", "Suppression d'un équipement", Colors.WARNING,51459),
+    KIT_UPDATED("", "Mise à jour d'un équipement", Colors.WARNING,51459),
+    ISLAND_MEMBER_ADDED("", "Ajout d'un membre d'île", Colors.COMMON,53343),
+    ISLAND_MEMBER_LEAVED("", "Suppression d'un membre d'île", Colors.COMMON,53343),
+    ISLAND_MEMBER_KICKED("", "Expulsion d'un membre d'île", Colors.COMMON,53343),
+    ISLAND_BANK_DEPOSIT("", "Dépôt d'argent sur banque d'île", Colors.COMMON,53343),
+    ISLAND_BANK_WITHDRAW("", "Retrait d'argent sur banque d'île", Colors.COMMON,53343),
+    ISLAND_BANK_DEPOSIT_FAILED("", "Dépôt d'argent sur banque d'île échoué", Colors.COMMON,53343),
+    ISLAND_BANK_WITHDRAW_FAILED("", "Retrait d'argent sur banque d'île échoué", Colors.COMMON,53343),
+    ISLAND_VISITOR_ADDED("", "Ajout d'un visiteur sur l'île", Colors.COMMON,53343),
+    ISLAND_VISITOR_REMOVED("", "Suppression d'un visiteur sur l'île", Colors.COMMON,53343),
+    ISLAND_VISITOR_KICKED("", "Expulsion d'un visiteur sur l'île", Colors.COMMON,53343),
+    ISLAND_VISITOR_BANNED("", "Bannissement d'un visiteur sur l'île", Colors.COMMON,53343),
+    ISLAND_VISITOR_UNBANNED("", "Débannissement d'un visiteur sur l'île", Colors.COMMON,53343),
+    ISLAND_WEATHER_CHANGED("", "Changement du temps de l'île", Colors.COMMON,34467),
+    ISLAND_TIME_CHANGED("", "Changement de l'heure de l'île", Colors.COMMON,8369);
 
     private final String itemDescription;
     private final String itemTitle;
