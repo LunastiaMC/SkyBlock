67c57b9f17127ce351e8ee84292d70efdb5428ef
Gaëtan H <steellgold0@gmail.com>
2022-08-12 19:13:06 +0200

    Envoi de logs

--- src/main/java/fr/lunastia/skyblock/core/session/server/logs/Log.java
+++ src/main/java/fr/lunastia/skyblock/core/session/server/logs/Log.java
@@ -10,7 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public interface Log {
-    default void send(EnumLogs type, Player target, Player moderator, String reason, String expireAt) {
+    default void sendModLog(EnumLogs type, Player target, Player moderator, String reason, String expireAt) {
         Core.getInstance().getServer().getScheduler().runTaskAsynchronously(Core.getInstance(), () -> {
             try {
                 Connection connection = Manager.getDatabaseManager().getDatabase().getConnection();
@@ -21,6 +21,21 @@ public interface Log {
                 statement.setString(4, moderator.getUniqueId().toString());
                 statement.setString(5, reason);
                 statement.setString(6, expireAt);
+                statement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    default void sendCommonLog(EnumLogs type, Player target) {
+        Core.getInstance().getServer().getScheduler().runTaskAsynchronously(Core.getInstance(), () -> {
+            try {
+                Connection connection = Manager.getDatabaseManager().getDatabase().getConnection();
+                final PreparedStatement statement = connection.prepareStatement("INSERT INTO logs (type, target, target_name) VALUES (?,?,?)");
+                statement.setString(1, type.toString());
+                statement.setString(2, target.getUniqueId().toString());
+                statement.setString(3, target.getName());
                 statement.execute();
             } catch (SQLException e) {
                 e.printStackTrace();
