60bd7fa3984883f4d40043d4371f4d92bcf3cbcc
Gaëtan H <steellgold0@gmail.com>
2022-08-12 16:52:07 +0200

    Ajout d'un GUI pour les logs

--- src/main/java/fr/lunastia/skyblock/core/gui/LogsGUI.java
+++ src/main/java/fr/lunastia/skyblock/core/gui/LogsGUI.java
@@ -1,14 +1,24 @@
 package fr.lunastia.skyblock.core.gui;
 
+import fr.lunastia.skyblock.core.manager.Manager;
+import fr.lunastia.skyblock.core.utils.ItemUtils;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LogsGUI implements GUI {
+
     @Override
     public String getName() {
         return null;
@@ -20,13 +30,20 @@ public class LogsGUI implements GUI {
     }
 
     @Override
-    public void getContents(Player player, Inventory inventory) {
-
+    public void getContents(Player player, Inventory inventory) throws SQLException {
+        Connection connection = Manager.getDatabaseManager().getDatabase().getConnection();
+        final PreparedStatement statement = connection.prepareStatement("SELECT * FROM logs LIMIT 10");
+        final ResultSet resultSet = statement.executeQuery();
+        int slot = 0;
+        while (resultSet.next()) {
+            ItemStack item = ItemUtils.customizedItem(getItem(resultSet.getString("target_name")), new ArrayList<>());
+            inventory.setItem(slot, item);
+            slot++;
+        }
     }
 
     @Override
     public void onClick(Player player, Inventory inventory, ItemStack itemStack, int slot, ClickType clickType) throws SQLException {
-
     }
 
     @Override
@@ -47,5 +64,24 @@ public class LogsGUI implements GUI {
     @Override
     public InventoryType getInventoryType() {
         return null;
+    }
+
+    public ItemStack getItem(String player) {
+        boolean isNewVersion = Arrays.stream(Material.values())
+                .map(Material::name).toList().contains("PLAYER_HEAD");
+
+        Material type = Material.matchMaterial(isNewVersion ? "PLAYER_HEAD" : "SKULL_ITEM");
+        assert type != null;
+        ItemStack item = new ItemStack(type);
+        if (!isNewVersion) {
+            item.setDurability((short) 3);
+        }
+
+        SkullMeta meta = (SkullMeta) item.getItemMeta();
+        assert meta != null;
+        // get player profile
+        meta.setOwner(player);
+        item.setItemMeta(meta);
+        return item;
     }
 }
