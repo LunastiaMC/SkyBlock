bddfe17f284ab41ab9591b6c7d2be410f708e204
Gaëtan H <steellgold0@gmail.com>
2022-08-12 18:02:31 +0200

    Modifications

--- src/main/java/fr/lunastia/skyblock/core/session/server/EnumLogs.java
+++ src/main/java/fr/lunastia/skyblock/core/session/server/EnumLogs.java
@@ -38,32 +38,12 @@ public enum EnumLogs {
     ISLAND_WEATHER_CHANGED("Changement du temps de l'île"),
     ISLAND_TIME_CHANGED("Changement de l'heure de l'île");
 
-    public static EnumLogs getByName(final String name) {
-        for (final EnumLogs enumLogs : values()) {
-            if (enumLogs.name().equalsIgnoreCase(name)) {
-                return enumLogs;
-            }
-        }
-        return null;
-    }
+    private final String itemTitle;
 
-    public static EnumLogs getByIndex(final int index) {
-        for (final EnumLogs enumLogs : values()) {
-            if (enumLogs.ordinal() == index) {
-                return enumLogs;
-            }
-        }
-        return null;
-    }
-
-    public static boolean isPlayerLog(final EnumLogs enumLogs) {
-        return enumLogs.ordinal() <= PLAYER_KIT_USE.ordinal();
     EnumLogs(String itemTitle) {
         this.itemTitle = itemTitle;
     }
 
-    public static boolean isIslandLog(final EnumLogs enumLogs) {
-        return enumLogs.ordinal() >= ISLAND_MEMBER_ADDED.ordinal();
     public String getItemTitle() {
         return itemTitle;
     }
