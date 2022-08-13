f96c05ebd03a9059ae0ce22523d06b1a8efeb6d4
Gaëtan H <steellgold0@gmail.com>
2022-08-13 14:46:03 +0200

    Ajout d'un identifiant unique au chapeau

--- src/main/java/fr/lunastia/skyblock/core/session/Hats.java
+++ src/main/java/fr/lunastia/skyblock/core/session/Hats.java
@@ -1,23 +1,31 @@
 package fr.lunastia.skyblock.core.session;
 
-public enum Hats {
-    GLASS("glass", "Chapeau en verre", "skyblock.hat.use"),
-    TINTED_GLASS("tinted_glass", "Chapeau en verre teinté", "skyblock.hat.use"),
-    WOOL("white_wool", "Chapeau en laine", "skyblock.hat.use"),
-    NETHERITE_BLOCK("netherite_block", "Chapeau de bloc de netherite", "skyblock.hat.use");
+import java.util.UUID;
 
+public enum Hats {
+    GLASS(UUID.fromString("f31f3518-19ab-499a-a49f-e185cc1b089b"), "glass", "Chapeau en verre", "skyblock.hat.use"),
+    TINTED_GLASS(UUID.fromString("fbd30c53-b6cc-40be-b96d-415fa6879844"), "tinted_glass", "Chapeau en verre teinté", "skyblock.hat.use"),
+    WOOL(UUID.fromString("b1863e8f-7150-4a13-a5bf-436212b216d1"), "white_wool", "Chapeau en laine", "skyblock.hat.use"),
+    NETHERITE_BLOCK(UUID.fromString("4214efdd-af74-493b-8924-396edb561240"), "netherite_block", "Chapeau de bloc de netherite", "skyblock.hat.use");
+
+    private final UUID uuid;
     private final String identifier;
     private final String displayName;
     private final String permission;
 
-    Hats(String identifier, String displayName, String permission) {
-        this.identifier = "minecraft:" + identifier;
+    Hats(UUID uuid, String minecraftIdentifier, String displayName, String permission) {
+        this.uuid = uuid;
+        this.identifier = "minecraft:" + minecraftIdentifier;
         this.displayName = displayName;
         this.permission = permission;
     }
 
     public static Hats[] getHats() {
         return Hats.values();
+    }
+
+    public UUID getUniqueId() {
+        return uuid;
     }
 
     public String getIdentifier() {
