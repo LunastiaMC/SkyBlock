7107b90ee929e1393611288e2fc167f62092b322
Gaëtan H <steellgold0@gmail.com>
2022-08-13 15:02:14 +0200

    Réécriture du GUI

--- src/main/java/fr/lunastia/skyblock/core/gui/HatListGUI.java
+++ src/main/java/fr/lunastia/skyblock/core/gui/HatListGUI.java
@@ -1,13 +1,12 @@
 package fr.lunastia.skyblock.core.gui;
 
-import fr.lunastia.skyblock.core.Core;
+import fr.lunastia.skyblock.core.manager.Manager;
 import fr.lunastia.skyblock.core.session.Hats;
+import fr.lunastia.skyblock.core.session.Session;
 import fr.lunastia.skyblock.core.utils.ItemUtils;
 import fr.lunastia.skyblock.core.utils.colors.ColorUtils;
 import fr.lunastia.skyblock.core.utils.colors.Colors;
 import org.bukkit.Material;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryType;
@@ -16,6 +15,7 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
 
 public class HatListGUI implements GUI {
     @Override
@@ -34,6 +34,7 @@ public class HatListGUI implements GUI {
         Hats[] hats = Hats.getHats();
         int i = 0;
         for (Hats hat : hats) {
+            if (i >= getSize()) return;
             ArrayList<String> lore = new ArrayList<>();
             if (!player.hasPermission(hat.getPermission())) {
                 lore.add("§c§l✘ §r§cVous n'avez pas débloqué ce chapeau");
@@ -44,67 +45,63 @@ public class HatListGUI implements GUI {
             i++;
 
             // Get item
-            ItemStack item = new ItemStack(Material.valueOf(hat.getIdentifier()));
+            ItemStack item = new ItemStack(Objects.requireNonNull(Material.matchMaterial(hat.getIdentifier())));
             inventory.setItem(i, ItemUtils.customizedItem(item, "§d" + hat.getDisplayName(), lore));
+        }
+
+        Session session = Manager.getSessionManager().getSession(player);
+        if (session == null) {
+            return;
+        }
+
+        if (session.hasHat()) {
+            inventory.setItem(18, ItemUtils.customizedItem(new ItemStack(Material.BARRIER), "§cRetirer le chapeau", new ArrayList<>()));
         }
     }
 
     @Override
     public void onClick(Player player, Inventory inventory, ItemStack itemStack, int slot, ClickType clickType) {
-        YamlConfiguration config = YamlConfiguration.loadConfiguration(Core.getInstance().getHatConfig());
-        ConfigurationSection section = config.getConfigurationSection("hats");
-
-        assert section != null;
-        if (section.contains(String.valueOf(slot))) {
-            if (player.hasPermission(section.getString(slot + ".permission"))) {
-                if (player.getInventory().getHelmet() != null) {
-                    if (player.getInventory().getHelmet().getItemMeta().getDisplayName().contains("Chapeau")) {
-                        String displayName = section.getString(slot + ".displayName");
-                        if (player.getInventory().getHelmet().getItemMeta().getDisplayName() == section.getString(slot + ".displayName")) {
-                            ColorUtils.sendMessage(player, "Vous avez deja le chapeau §d" + displayName, Colors.HAT);
-                            return;
-                        }
-                        ColorUtils.sendMessage(player, "Vous venez d'appliquer le chapeau §d" + displayName, Colors.HAT);
-                        ItemStack item = ItemUtils.customizedItem(new ItemStack(Material.matchMaterial(section.getString(String.valueOf(slot) + ".id"))), section.getString(String.valueOf(slot) + ".displayName"), new ArrayList<>());
-                        player.getInventory().setHelmet(item);
-
-                        LogTypeCommon log = new LogTypeCommon(EnumLogs.PLAYER_CHANGE_HAT, player, displayName);
-                        log.setStartDate(new Date().toString());
-                        log.send();
-                        return;
-                    }
-                    ColorUtils.sendMessage(player, "Veuillez enlevez votre casque avant de mettre un chapeau !", Colors.HAT);
-                    return;
-                }
-
-                String displayName = section.getString(slot + ".displayName");
-                ItemStack item = ItemUtils.customizedItem(new ItemStack(Material.matchMaterial(section.getString(String.valueOf(slot) + ".id"))), section.getString(String.valueOf(slot) + ".displayName"), new ArrayList<>());
-
-                ColorUtils.sendMessage(player, "Vous venez d'appliquer le chapeau §d" + displayName, Colors.HAT);
-                player.getInventory().setHelmet(item);
-            } else {
-                ColorUtils.sendMessage(player, "Vous n'avez pas encore débloqué ce chapeau !", Colors.HAT, true);
+            Hats hat = Hats.getHat(itemStack.getType());
+            if (hat == null) {
+                return;
             }
+
+            if (!player.hasPermission(hat.getPermission())) {
+                ColorUtils.sendMessage(player, "Vous n'avez pas débloqué ce chapeau", Colors.HAT, true);
+                return;
+            }
+
+            Session session = Manager.getSessionManager().getSession(player);
+            if (session == null) {
+                return;
+            }
+
+            if (session.getHat() != null) {
+                ColorUtils.sendMessage(player, "Vous avez déjà un chapeau, veuillez le retirer pour en choisir un autre", Colors.HAT, true);
+                return;
+            }
+
+            session.setHat(hat, itemStack);
+            ColorUtils.sendMessage(player, "Vous venez d'appliquer le chapeau" + hat.getDisplayName(), Colors.HAT);
+        }
+
+        @Override
+        public void onClose (Player player, Inventory inventory){
+            return;
+        }
+
+        @Override
+        public void onOpen (Player player, Inventory inventory){
+            return;
+        }
+
+        @Override
+        public boolean clickCancelled () {
+            return true;
+        }
+
+        @Override
+        public InventoryType getInventoryType () {
+            return null;
         }
     }
-
-    @Override
-    public void onClose(Player player, Inventory inventory) {
-        return;
-    }
-
-    @Override
-    public void onOpen(Player player, Inventory inventory) {
-        return;
-    }
-
-    @Override
-    public boolean clickCancelled() {
-        return true;
-    }
-
-    @Override
-    public InventoryType getInventoryType() {
-        return null;
-    }
-}
