d4fad1dd1b3df04c28358f4d5fc7692bee026c0f
Gaëtan H <steellgold0@gmail.com>
2022-08-14 12:43:22 +0200

    Envoi de fichier journaux (/vanish, /freeze)

--- src/main/java/fr/lunastia/skyblock/core/commands/moderation/VanishCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/moderation/VanishCommand.java
@@ -6,9 +6,14 @@ import dev.jorel.commandapi.annotations.Permission;
 import dev.jorel.commandapi.annotations.Subcommand;
 import fr.lunastia.skyblock.core.manager.Manager;
 import fr.lunastia.skyblock.core.session.Session;
+import fr.lunastia.skyblock.core.session.server.EnumLogs;
+import fr.lunastia.skyblock.core.session.server.logs.LogTypeModeration;
 import fr.lunastia.skyblock.core.utils.colors.ColorUtils;
 import fr.lunastia.skyblock.core.utils.colors.Colors;
 import org.bukkit.entity.Player;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Command("vanish")
 @Permission("skyblock.vanish.command")
@@ -16,6 +21,11 @@ public class VanishCommand {
     @Default
     public static void vanish(Player player) {
         Session session = Manager.getSessionManager().getSession(player);
+
+        LogTypeModeration log = new LogTypeModeration(!session.isVanished() ? EnumLogs.PLAYER_VANISHED : EnumLogs.PLAYER_UNVANISHED, null, player, null);
+        log.setStartAt(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
+        log.send();
+
         session.setVanished(!session.isVanished(), true);
     }
 
