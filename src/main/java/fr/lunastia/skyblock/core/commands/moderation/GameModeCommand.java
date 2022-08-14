268a452b0b5e7f269ae5176bedebaf1be71b9069
Gaëtan H <steellgold0@gmail.com>
2022-08-14 02:11:20 +0200

    Ajout d'un log type: Gamemode & Money

--- src/main/java/fr/lunastia/skyblock/core/commands/moderation/GameModeCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/moderation/GameModeCommand.java
@@ -7,6 +7,8 @@ import dev.jorel.commandapi.annotations.Permission;
 import dev.jorel.commandapi.annotations.arguments.AMultiLiteralArgument;
 import dev.jorel.commandapi.annotations.arguments.APlayerArgument;
 import fr.lunastia.skyblock.core.Core;
+import fr.lunastia.skyblock.core.session.server.EnumLogs;
+import fr.lunastia.skyblock.core.session.server.logs.LogTypeGamemode;
 import fr.lunastia.skyblock.core.utils.colors.ColorUtils;
 import fr.lunastia.skyblock.core.utils.colors.Colors;
 import org.bukkit.configuration.ConfigurationSection;
@@ -22,6 +24,14 @@ public class GameModeCommand {
 
     @Default
     public static void gamemode(Player player, @AMultiLiteralArgument({"creative", "1", "survival", "0", "adventure", "2", "spectator", "3"}) String mode) {
+        LogTypeGamemode log = new LogTypeGamemode(EnumLogs.PLAYER_CHANGE_GAMEMODE, player, null, player.getGameMode(), switch (mode) {
+            case "creative", "1" -> org.bukkit.GameMode.CREATIVE;
+            case "adventure", "2" -> org.bukkit.GameMode.ADVENTURE;
+            case "spectator", "3" -> org.bukkit.GameMode.SPECTATOR;
+            default -> org.bukkit.GameMode.SURVIVAL;
+        });
+        log.send();
+
         ColorUtils.sendMessage(player, "Vous avez changé votre mode de jeu en §f" + getNameGameMode(mode), Colors.PREFIX);
         changeGamemode(player, mode);
     }
@@ -29,6 +39,14 @@ public class GameModeCommand {
     @Default
     @Permission("skyblock.gamemode.other.command")
     public static void gamemode(Player player, @APlayerArgument Player target, @AMultiLiteralArgument({"creative", "1", "survival", "0", "adventure", "2", "spectator", "3"}) String mode) {
+        LogTypeGamemode log = new LogTypeGamemode(EnumLogs.PLAYER_GAMEMODE_CHANGED_BY_FORCE, target, player, target.getGameMode(), switch (mode) {
+            case "creative", "1" -> org.bukkit.GameMode.CREATIVE;
+            case "adventure", "2" -> org.bukkit.GameMode.ADVENTURE;
+            case "spectator", "3" -> org.bukkit.GameMode.SPECTATOR;
+            default -> org.bukkit.GameMode.SURVIVAL;
+        });
+        log.send();
+
         changeGamemode(target, mode);
 
         ColorUtils.sendMessage(target, "Votre mode de jeu a été modifié en §f" + getNameGameMode(mode) + "§7 par §f" + player.getName(), Colors.PREFIX);
--- src/main/java/fr/lunastia/skyblock/core/session/server/logs/Log.java
+++ src/main/java/fr/lunastia/skyblock/core/session/server/logs/Log.java
@@ -9,8 +9,7 @@ import org.bukkit.entity.Player;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.Types;
 
 public interface Log {
     default void sendModLog(EnumLogs type, Player target, Player moderator, String reason, String startAt, String expireAt) {
@@ -50,18 +49,56 @@ public interface Log {
         });
     }
 
-    default void sendEconomyLog(EnumLogs type, Player target, Integer oldBalance, Integer newBalance, Integer amount) {
+    default void sendCommonLog(EnumLogs type, Player target, Player moderator, String startAt, Integer oldGamemode, Integer newGamemode) {
         Core.getInstance().getServer().getScheduler().runTaskAsynchronously(Core.getInstance(), () -> {
             try {
                 Connection connection = Manager.getDatabaseManager().getDatabase().getConnection();
-                final PreparedStatement statement = connection.prepareStatement("INSERT INTO logs (type, target, target_name, startAt, balance_old, balance_new, balance_transaction) VALUES (?,?,?,?,?,?,?)");
+                final PreparedStatement statement = connection.prepareStatement("INSERT INTO logs (type, target, target_name, moderator, moderator_name, startAt, old_gamemode, new_gamemode) VALUES (?,?,?,?,?,?,?,?)");
                 statement.setString(1, type.toString());
                 statement.setString(2, target.getUniqueId().toString());
                 statement.setString(3, target.getName());
-                statement.setString(4, ModerationManager.getMinimizedDate(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date())));
-                statement.setInt(5, oldBalance);
-                statement.setInt(6, newBalance);
-                statement.setInt(7, amount);
+                if (moderator != null) {
+                    statement.setString(4, moderator.getUniqueId().toString());
+                    statement.setString(5, moderator.getName());
+                } else {
+                    statement.setNull(4, Types.NULL);
+                    statement.setNull(5, Types.NULL);
+                }
+                statement.setString(6, startAt);
+                statement.setInt(7, oldGamemode);
+                statement.setInt(8, newGamemode);
+                statement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    default void sendEconomyLog(EnumLogs type, Player target, Player moderator, Integer oldBalance, Integer newBalance, Integer amount, String transactionTarget, EnumLogs transactionType, String startAt) {
+        Core.getInstance().getServer().getScheduler().runTaskAsynchronously(Core.getInstance(), () -> {
+            try {
+                Connection connection = Manager.getDatabaseManager().getDatabase().getConnection();
+                final PreparedStatement statement = connection.prepareStatement("INSERT INTO logs (type, target, target_name, moderator, moderator_name, startAt, balance_old, balance_new, balance_transaction, balance_transaction_target, balance_transaction_type) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+                statement.setString(1, type.toString());
+                statement.setString(2, target.getUniqueId().toString());
+                statement.setString(3, target.getName());
+                if (moderator != null) {
+                    statement.setString(4, moderator.getUniqueId().toString());
+                    statement.setString(5, moderator.getName());
+                } else {
+                    statement.setNull(4, Types.NULL);
+                    statement.setNull(5, Types.NULL);
+                }
+                statement.setString(6, startAt);
+                statement.setInt(7, oldBalance);
+                statement.setInt(8, newBalance);
+                statement.setInt(9, amount);
+                if (transactionTarget == null) {
+                    statement.setNull(10, Types.NULL);
+                } else {
+                    statement.setString(10, transactionTarget);
+                }
+                statement.setString(11, transactionType.toString());
                 statement.execute();
             } catch (SQLException e) {
                 e.printStackTrace();
