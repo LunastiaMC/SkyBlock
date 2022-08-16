f8dd48348d4964737e0a8a6c89e46f2e534172a8
Gaëtan H <steellgold0@gmail.com>
2022-08-17 01:44:52 +0200

    Ouverture du GUI

--- src/main/java/fr/lunastia/skyblock/core/commands/island/IslandCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/island/IslandCommand.java
@@ -4,12 +4,12 @@ import dev.jorel.commandapi.annotations.Command;
 import dev.jorel.commandapi.annotations.Default;
 import dev.jorel.commandapi.annotations.Subcommand;
 import fr.lunastia.skyblock.core.gui.island.IslandCreationGUI;
-import fr.lunastia.skyblock.core.manager.GUIManager;
 import fr.lunastia.skyblock.core.manager.Manager;
 import fr.lunastia.skyblock.core.session.Session;
 import org.bukkit.entity.Player;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 
 @Command("island")
 public class IslandCommand {
@@ -19,9 +19,15 @@ public class IslandCommand {
     }
 
     @Subcommand("create")
-    public static void create(Player player) throws SQLException {
+    public static void create(Player player) {
         Session session = Manager.getSessionManager().getSession(player);
-        Manager.getGUIManager().open(player, IslandCreationGUI.class);
+        try {
+            HashMap<Integer, String> argument = new HashMap<>();
+            argument.put(0, "easy");
+            Manager.getGUIManager().open(player, IslandCreationGUI.class, argument);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Subcommand("disband")
