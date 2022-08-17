76351ca9f9e0a7415691931ea4fddbf047809b09
Gaëtan H <steellgold0@gmail.com>
2022-08-17 19:50:46 +0200

    Modifications de la commande /island

--- src/main/java/fr/lunastia/skyblock/core/commands/island/IslandCommand.java
+++ src/main/java/fr/lunastia/skyblock/core/commands/island/IslandCommand.java
@@ -6,6 +6,8 @@ import dev.jorel.commandapi.annotations.Subcommand;
 import fr.lunastia.skyblock.core.gui.island.IslandCreationGUI;
 import fr.lunastia.skyblock.core.manager.Manager;
 import fr.lunastia.skyblock.core.session.Session;
+import fr.lunastia.skyblock.core.utils.colors.ColorUtils;
+import fr.lunastia.skyblock.core.utils.colors.Colors;
 import org.bukkit.entity.Player;
 
 import java.sql.SQLException;
@@ -16,11 +18,20 @@ public class IslandCommand {
     @Default
     public static void island(Player player) {
         Session session = Manager.getSessionManager().getSession(player);
+        if (!session.hasIsland()) {
+            ColorUtils.sendMessage(player, "Vous n'avez pas d'île, créez-en une avec /island create", Colors.PREFIX, true);
+            return;
+        }
     }
 
     @Subcommand("create")
     public static void create(Player player) {
         Session session = Manager.getSessionManager().getSession(player);
+        if (session.hasIsland()) {
+            ColorUtils.sendMessage(player, "Vous avez déjà une île, supprimez-la ou archiver la pour créer une nouvelle.", Colors.PREFIX, true);
+            return;
+        }
+
         try {
             HashMap<Integer, String> argument = new HashMap<>();
             argument.put(0, "easy");
@@ -30,8 +41,21 @@ public class IslandCommand {
         }
     }
 
+    @Subcommand("menu")
+    public static void menu(Player player) {
+        Session session = Manager.getSessionManager().getSession(player);
+        if (!session.hasIsland()) {
+            ColorUtils.sendMessage(player, "Vous n'avez pas d'île, créez-en une avec /island create", Colors.PREFIX, true);
+            return;
+        }
+    }
+
     @Subcommand("disband")
     public static void disband(Player player) {
         Session session = Manager.getSessionManager().getSession(player);
+        if (!session.hasIsland()) {
+            ColorUtils.sendMessage(player, "Vous n'avez pas d'île, créez-en une avec /island create", Colors.PREFIX, true);
+            return;
+        }
     }
 }
