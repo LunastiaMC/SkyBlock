879337da0490042f30c3f84a6e5c8ae604191398
Gaëtan H <steellgold0@gmail.com>
2022-08-17 19:51:18 +0200

    Ajout d'un type d'île

---
+++ src/main/java/fr/lunastia/skyblock/core/island/types/SleepingForest.java
@@ -0,0 +1,11 @@
+package fr.lunastia.skyblock.core.island.types;
+
+import fr.lunastia.skyblock.core.session.Island;
+
+import java.util.UUID;
+
+public class SleepingForest extends Island {
+    public SleepingForest(UUID uuid) {
+        super(uuid);
+    }
+}
