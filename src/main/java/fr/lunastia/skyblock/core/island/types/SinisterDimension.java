2a23aaa3a32e79732d61ec175e89eb87b11f948c
Gaëtan H <steellgold0@gmail.com>
2022-08-18 12:48:39 +0200

    Ajout de: Dimension sinistre

---
+++ src/main/java/fr/lunastia/skyblock/core/island/types/SinisterDimension.java
@@ -0,0 +1,13 @@
+package fr.lunastia.skyblock.core.island.types;
+
+import fr.lunastia.skyblock.core.session.Island;
+
+import java.util.UUID;
+
+public class SinisterDimension extends Island {
+    public SinisterDimension(UUID uuid) {
+        super(uuid);
+        setDisplayName("&#7455afD&#7555b0i&#7655b1m&#7754b2e&#7954b4n&#7a54b5s&#7b54b6i&#7c54b7o&#7d54b8n &#7e53b9s&#7f53bai&#8053bbn&#8253bdi&#8353bes&#8452bft&#8552c0r&#8652c1e");
+        setName("Dimension sinistre");
+    }
+}
